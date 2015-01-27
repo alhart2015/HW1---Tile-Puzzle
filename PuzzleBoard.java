076185ae79022fbc55c1fdea1c748e788a030d1e
alhart2015 <alhart@davidson.edu>
2015-01-26 23:04:38 -0500

    Deleted commented-out dead code to clean it up.

--- PuzzleBoard.java
+++ PuzzleBoard.java
@@ -19,8 +19,6 @@ public class PuzzleBoard {
 
     private Tile[][] board;
     private int size;
-    // private int blankRow; // Unconvinced I want/need these
-    // private int blankCol;
 
     // if parity + d(blank, goalBlank) is odd, board isn't solvable
 
@@ -54,8 +52,7 @@ public class PuzzleBoard {
         this.board[2][2] = new Tile(2, 2, i);
     }
 
-    /* Creates a random solvable board using the algorithm described at
-    http://cseweb.ucsd.edu/~ccalabro/essays/15_puzzle.pdf
+    /* Creates a random solvable board using the parity sign check.
     */
     private void init() {
         boolean valid = false;
@@ -63,18 +60,17 @@ public class PuzzleBoard {
             this.makeRandomBoard();
             valid = this.isValid();
         }
-        System.out.println(this.parity());
-        System.out.println(this.zeroDistance());
     }
 
-    /* Checks if the current board is valid */
+    /* Checks if the current board is valid.
+    */
     private boolean isValid() {
         int parity = this.parity();
         int dist = this.zeroDistance();
         return parity % 2 == dist % 2;
     }
 
-    /* Makes a random board
+    /* Makes a random board, may not be solvable.
     */
     private void makeRandomBoard() {
         Random rand = new Random();
@@ -99,22 +95,18 @@ public class PuzzleBoard {
     }
 
     /* Calculates the parity of the board, ie the number of swaps needed
-    to get from the starting board to the solved board
+    to get from the starting board to the solved board.
     */
     private int parity() {
 
         // Make lists instead of matrices for ease of computing parity
         int len = this.size * this.size;
-
-        // List of goal state: [0, 1, ..., n^2]
-        int[] goal = new int[len];
         int[] state = new int[len];
 
         int numSwaps = 0;
         int numCorrect = 0;
 
         for (int i = 0; i < len; i++) {
-            goal[i] = i;
             state[i] = this.board[i/this.size][i%this.size].getVal();
             if (i == state[i]) {
                 numCorrect++;
@@ -155,7 +147,7 @@ public class PuzzleBoard {
     }
 
     /* Swap the element of array a in position x with the element in
-    position y
+    position y.
     */
     private static int[] swap(int[] a, int x, int y) {
         int temp = a[x];
@@ -168,123 +160,6 @@ public class PuzzleBoard {
     public void move() {
 
     }
-
-    // /* Creates a random state of the board. Creates a solved board and then
-    // makes a random number of random moves, shuffling the board.
-    // */
-    // private void init() {
-
-    //     Random rand = new Random();
-
-    //     for (int i = 0; i < this.size; i++) {
-    //         for (int j = 0; j < this.size; j++) {
-    //             // Start with a solved board
-    //             // this.board[i][j] = this.size*i + j;
-    //             this.board[i][j] = new Tile(i, j, this.size*i + j);
-    //         }
-    //     }
-
-    //     // how many random moves to make? Tweak this to get a good
-    //     // distribution of random boards
-    //     int randRange = 100;
-    //     int numMoves = rand.nextInt(randRange);
-    //     // lastMoved[0] = position of the empty space
-    //     // lastMoved[1] = position of the last Tile moved
-    //     Tile[] lastMoved = new Tile[2];
-    //     lastMoved[EMPTY_SPACE] = this.board[0][0];
-    //     // Pick any tile that can't be moved the first time for this
-    //     lastMoved[LAST_MOVED] = this.board[2][2];
-    //     for (int i = 0; i < numMoves; i++) {
-    //         lastMoved = randomMove(lastMoved);
-    //     }
-
-    // }
-
-    // /* Helper for init(), makes a random move on the board. As above,
-    // lastMoved[0] = the position of the empty space, and
-    // lastMoved[1] = the position of the last Tile moved. We track these
-    // so that your move cannot undo your previous move, which prevents
-    // the space of likely boards from being too small.
-    // */
-    // private Tile[] randomMove(Tile[] lastMoved) {
-    //     /* Notes:
-    //         - remember where you just moved so you can't do that
-    //         - keep track of where the 0 is so you don't have to loop
-    //             through the whole board every time
-    //     */
-
-    //     Random rand = new Random();
-
-    //     Tile empty = lastMoved[EMPTY_SPACE];
-
-    //     List<Tile> nbd = this.neighborhood(empty.getRow(), empty.getCol());
-    //     Collections.shuffle(nbd);
-    //     int index = 0;
-    //     Tile toMove = nbd.get(index);   // Check the first thing
-    //     if (toMove == lastMoved[LAST_MOVED]) {
-    //         // The first thing in the shuffled neighborhood was the last tile
-    //         // you moved. Can't use it, so use the next one.
-    //         index++;
-    //         toMove = nbd.get(index);
-    //         // Note that the neighborhood will always have at least two
-    //         // elements, so this will never throw an exception
-    //     }
-
-    //     // System.out.println(toMove);
-    //     System.out.println("toMove: " + toMove + 
-    //         toMove.getRow() + toMove.getCol());
-    //     System.out.println("blank: " + empty + 
-    //         empty.getRow() + empty.getCol());
-    //     System.out.println("lastMoved: " + lastMoved[LAST_MOVED] + 
-    //         lastMoved[LAST_MOVED].getRow() + lastMoved[LAST_MOVED].getCol());
-
-    //     lastMoved = this.move(toMove.getRow(), toMove.getCol(), 
-    //                empty.getRow(), empty.getCol(), lastMoved);
-
-    //     return lastMoved;
-    // }
-
-    // /* Moves the tile in position (r, c) into the empty space */
-    // public Tile[] move(int r, int c, int blankR, int blankC, Tile[] moved) {
-    //     // if (this.moveExists(this.board[r][c].getVal())) {
-    //     //     Tile temp = this.board[r][c];
-    //     // }
-
-    //     // Not sure you need to check if the move exists, since you're
-    //     // only calling the move() method on tiles in the neightborhood
-    //     // of the empty space
-    //     Tile temp = this.board[r][c];
-    //     this.board[r][c] = this.board[blankR][blankC];
-    //     moved[EMPTY_SPACE] = this.board[r][c];
-    //     this.board[blankR][blankC] = this.board[r][c];
-    //     moved[LAST_MOVED] = this.board[blankR][blankC];
-    //     return moved;
-    // }
-
-    // /* Checks if a move exists from the given number */
-    // public boolean moveExists(int num) {
-
-    //     for (int i = 0; i < this.size; i++) {
-    //         for (int j = 0; j < this.size; j++) {
-    //             Tile current = this.board[i][j];
-    //             if (num == current.getVal()) {
-    //                 // You've found the number you're worried about, so check
-    //                 // if the empty space is next to it. Unfortunately you
-    //                 // have to iterate through it, so we lose the efficiency
-    //                 // of checking set containment. There are a max of 4
-    //                 // elements here though, so it shouldn't be that bad
-    //                 List<Tile> nbd = this.neighborhood(i, j);
-    //                 for (Tile t : nbd) {
-    //                     if (t.isEmptySpace()) {
-    //                         return true;
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    //     return false;
-    // }
 
     /* Helper function for moveExists, returns the numbers to the left, right,
     above, and below the given (row, column), if they exist.
