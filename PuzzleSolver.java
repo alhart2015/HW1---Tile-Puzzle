ceec94e1610892d31c1cfa1c384726f3362355dc
alhart2015 <alhart@davidson.edu>
2015-01-29 15:56:08 -0500

    Updated README, and changed to generate 1200 random boards, instead of 100 at each depth.

--- PuzzleSolver.java
+++ PuzzleSolver.java
@@ -5,8 +5,10 @@ Alden Hart and Spencer Chadinha
 */
 
 import java.util.List;
-// import java.util.ArrayList;
+import java.util.ArrayList;
 // import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.Queue;
@@ -151,64 +153,127 @@ public class PuzzleSolver {
         }
     }
 
+    // public static void runTest() {
+
+    //     int[] h1depths = new int[12];
+    //     int[] h1branch = new int[12];
+    //     int[] h2depths = new int[12];
+    //     int[] h2branch = new int[12];
+
+    //     int[] h1depthAvg = new int[12];
+    //     int[] h1branchAvg = new int[12];
+    //     int[] h2depthAvg = new int[12];
+    //     int[] h2branchAvg = new int[12];
+
+    //     System.out.println("d\th1 b*\th2 b*");
+
+    //     for (int i = 1; i <= 12; i++) {
+    //         int depth = 2*i;
+    //         int h1d = 0;
+    //         int h1b = 0;
+    //         int h2d = 0;
+    //         int h2b = 0;
+    //         for (int j = 0; j < 100; j++) {
+    //             PuzzleBoard b1 = new PuzzleBoard(3, depth);
+    //             PuzzleBoard b2 = b1.copy();
+    //             int[] r1 = solve(b1, 1);
+    //             int[] r2 = solve(b2, 2);
+    //             h1d += r1[0];
+    //             h1b += r1[1];
+    //             h2d += r2[0];
+    //             h2b += r2[1];
+    //             // h1depths[i] = r1[0];
+    //             // h1branch[i] = r1[1];
+    //             // h2depths[i] = r2[0];
+    //             // h2branch[i] = r2[1];
+    //         }
+    //         // int h1d = 0;
+    //         // int h1b = 0;
+    //         // int h2d = 0;
+    //         // int h2b = 0;
+    //         // for (int j = 0; j < 100; j++) {
+    //         //     h1d += h1depths.get(j);
+    //         //     h1b += h1branch.get(j);
+    //         //     h2d += h2depths.get(j);
+    //         //     h2b += h2branch.get(j);
+    //         // }
+    //         h1depthAvg[i-1] = h1d/100;
+    //         h1branchAvg[i-1] = h1b/100;
+    //         h2depthAvg[i-1] = h2d/100;
+    //         h2branchAvg[i-1] = h2b/100;
+
+    //         System.out.print(depth + "\t");
+    //         System.out.print(h1branchAvg[i-1] + "\t");
+    //         System.out.println(h2branchAvg[i-1]);
+
+    //         // System.out.println("Depth: " + depth);
+    //         // System.out.println("Avg h1 depth: " + h1depthAvg[i-1]);
+    //         // System.out.println("Avg h1 b*: " + h1branchAvg[i-1]);
+    //         // System.out.println("Avg h2 depth: " + h2depthAvg[i-1]);
+    //         // System.out.println("Avg h2 b*: " + h2branchAvg[i-1]);
+    //     }
+    // }
+
+    /* Randomly generates 1200 boards and solves them, reporting the
+    depth and number of nodes generated
+    */
     public static void runTest() {
 
-        int[] h1depths = new int[12];
-        int[] h1branch = new int[12];
-        int[] h2depths = new int[12];
-        int[] h2branch = new int[12];
+        int numTrials = 1200;
 
-        int[] h1depthAvg = new int[12];
-        int[] h1branchAvg = new int[12];
-        int[] h2depthAvg = new int[12];
-        int[] h2branchAvg = new int[12];
+        // int[] h1Depths = new int[numTrials];
+        // int[] h1NodesGenerated = new int[numTrials];
+        // int[] h2Depths = new int[numTrials];
+        // int[] h2NodesGenerated = new int[numTrials];
 
-        System.out.println("d\th1 b*\th2 b*");
+        Map<Integer, List<Integer>> h1DepthMap = new HashMap<Integer, List<Integer>>();
+        Map<Integer, List<Integer>> h2DepthMap = new HashMap<Integer, List<Integer>>();
 
-        for (int i = 1; i <= 12; i++) {
-            int depth = 2*i;
-            int h1d = 0;
-            int h1b = 0;
-            int h2d = 0;
-            int h2b = 0;
-            for (int j = 0; j < 100; j++) {
-                PuzzleBoard b1 = new PuzzleBoard(3, depth);
-                PuzzleBoard b2 = b1.copy();
-                int[] r1 = solve(b1, 1);
-                int[] r2 = solve(b2, 2);
-                h1d += r1[0];
-                h1b += r1[1];
-                h2d += r2[0];
-                h2b += r2[1];
-                // h1depths[i] = r1[0];
-                // h1branch[i] = r1[1];
-                // h2depths[i] = r2[0];
-                // h2branch[i] = r2[1];
+        System.out.println("Generating and solving boards:");
+
+        for (int i = 0; i < numTrials; i++) {
+
+            PuzzleBoard b1 = new PuzzleBoard(3);
+            PuzzleBoard b2 = b1.copy();
+            int[] r1 = solve(b1, 1);
+            int[] r2 = solve(b2, 2);
+            int h1Depth = r1[0];
+            int h1Nodes = r1[1];
+            int h2Depth = r2[0];
+            int h2Nodes = r2[1];
+            if (!h1DepthMap.containsKey(h1Depth)) {
+                h1DepthMap.put(h1Depth, new ArrayList<Integer>());
             }
-            // int h1d = 0;
-            // int h1b = 0;
-            // int h2d = 0;
-            // int h2b = 0;
-            // for (int j = 0; j < 100; j++) {
-            //     h1d += h1depths.get(j);
-            //     h1b += h1branch.get(j);
-            //     h2d += h2depths.get(j);
-            //     h2b += h2branch.get(j);
-            // }
-            h1depthAvg[i-1] = h1d/100;
-            h1branchAvg[i-1] = h1b/100;
-            h2depthAvg[i-1] = h2d/100;
-            h2branchAvg[i-1] = h2b/100;
+            h1DepthMap.get(h1Depth).add(h1Nodes);
+            if (!h2DepthMap.containsKey(h2Depth)) {
+                h2DepthMap.put(h2Depth, new ArrayList<Integer>());
+            }
+            h2DepthMap.get(h2Depth).add(h2Nodes);
+            if (i % 100 == 0) {
+                System.out.println("Finished " + i + " boards.");
+            }
+        }
+        System.out.println("Done generating and solving boards, printing results:");
+        System.out.println("\nd\th1 b*\th2 b*");
+        // Iterate through the keys and print the average depths
+        Set<Integer> h1KeySet = h1DepthMap.keySet();
 
-            System.out.print(depth + "\t");
-            System.out.print(h1branchAvg[i-1] + "\t");
-            System.out.println(h2branchAvg[i-1]);
-
-            // System.out.println("Depth: " + depth);
-            // System.out.println("Avg h1 depth: " + h1depthAvg[i-1]);
-            // System.out.println("Avg h1 b*: " + h1branchAvg[i-1]);
-            // System.out.println("Avg h2 depth: " + h2depthAvg[i-1]);
-            // System.out.println("Avg h2 b*: " + h2branchAvg[i-1]);
+        for (Integer i : h1KeySet) {
+            int h1Avg = 0;
+            int h2Avg = 0;
+            List<Integer> h1Vals = h1DepthMap.get(i);
+            for (Integer n : h1Vals) {
+                h1Avg += n;
+            }
+            h1Avg = h1Avg / h1Vals.size();
+            if (h2DepthMap.containsKey(i)) {
+                List<Integer> h2Vals = h2DepthMap.get(i);
+                for (Integer n : h2Vals) {
+                    h2Avg += n;
+                }
+                h2Avg = h2Avg / h2Vals.size();
+            }
+            System.out.print(i + "\t" + h1Avg + "\t" + h2Avg + "\n");
         }
     }
 }
--- README.md
+++ README.md
@@ -3,15 +3,8 @@
 Solves an arbitrarily-sized N-Tile puzzle (though intended to solve
 the 8-Tile puzzle). To compile and run from a UNIX shell, run
 
-javac *.java; java PuzzleSolver x
+javac *.java; java PuzzleSolver
 
-where x is the number of the heuristic function you want to use. The heuristics
-are defined as follows:
-
-    1 chooses the misplaced tiles heuristic, which counts the number of tiles
-    that are not in their proper position.
-
-    2 chooses the Manhattan distance heuristic, which sums the Manhattan distance
-    of each tile on the board from its position in a solved board
-
-In the case of absent or invalid input, the misplaced tiles heuristic is used.
+This command generates 1200 random boards and solves them with A*,
+reporting the average number of nodes generated and the effective
+branching factor of each depth level.
